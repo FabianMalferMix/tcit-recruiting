59743a84fb5fff095fa399185a829a19b760a527
Fabian <fmalfer@luminux.cl>
2024-11-27 23:58:21 -0300

    Crea los primeros divs

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,19 @@
 import './App.css';
 
+import SearchBar from './components/searchBar/SearchBar';
+import PostsList from './components/postsList/PostsList';
+import PostsMaker from './components/postMaker/PostMaker';
+
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <p>
-          Hola Mundo!!
-        </p>
-      </header>
+{/*  */}
+      <div className='container-grid'>
+        <div className='cell'> <SearchBar/> </div>
+        <div className='cell'> <PostsList/> </div>
+        <div className='cell'> <PostsMaker/> </div>
+      </div>
+
     </div>
   );
 }
---
+++ frontend/src/components/postMaker/PostMaker.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './PostMaker.css';
+
+const PostsMaker = () => {
+    return(
+        <div className='post-maker-container'>
+            <h1>PostsMaker</h1>
+        </div>
+    )
+};
+// 
+export default PostsMaker
