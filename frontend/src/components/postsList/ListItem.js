c84050963edd1a7a145df0b97f927ebfbaba0bda
Fabian <fmalfer@luminux.cl>
2024-11-28 16:03:01 -0300

    Implementación button Eliminar post

--- frontend/src/components/postsList/ListItem.js
+++ frontend/src/components/postsList/ListItem.js
@@ -2,12 +2,22 @@ import React from "react";
 import './ListItem.css';
 
 const ListItem = props => {
+
+    const clickHandler = (e) => {
+        e.preventDefault();
+        console.log('Eliminar post')
+    }
+
     return(
-        <li className="posts-list-item">
+        <li key={props.id} className="posts-list-item">
             <div className="list-item-grid">
                 <div className="list-item-cell-name">{props.name}</div>
                 <div className="list-item-cell-desc">{props.description}</div>
-                <div className="list-item-cell-button">Boton</div>
+                <div className="list-item-cell-button">
+                    <button type="submit" className="delete-post-button" onClick={clickHandler}>
+                        Eliminar
+                    </button>
+                </div>
             </div>
         </li>
     )
