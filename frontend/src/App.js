2e9ebc8db75156f1d2ac76eca2f91f5d7448e5e0
Fabian <fmalfer@luminux.cl>
2024-11-28 15:27:20 -0300

    Agrega clase para celda de PostsList

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -9,7 +9,7 @@ function App() {
     <div className="App">
       <div className='container-grid'>
         <div className='cell'> <SearchBar/> </div>
-        <div className='cell'> <PostsList/> </div>
+        <div className='cell posts-list'> <PostsList/> </div>
         <div className='cell'> <PostsMaker/> </div>
       </div>
 
