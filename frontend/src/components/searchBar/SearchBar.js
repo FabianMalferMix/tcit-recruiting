75889e057b14e2c7e775d76135af9891c326e06c
Fabian <fmalfer@luminux.cl>
2024-11-29 01:43:02 -0300

    Implementa estado global en SearchBar

--- frontend/src/components/searchBar/SearchBar.js
+++ frontend/src/components/searchBar/SearchBar.js
@@ -1,12 +1,20 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import './SearchBar.css';
+import { useDispatch, useSelector } from 'react-redux';
+import { setSearchAction } from '../../redux/actions/searchActions';
 
 const SearchBar = () => {
 
+    const dispatch = useDispatch()
+    // const searchValue = useSelector(state => state.searchValue)
+
     const [inputValue, setInputValue] = useState("");
+
     const submitHandler = (e) => {
         e.preventDefault();
+        dispatch(setSearchAction(inputValue))
         console.log('Búsqueda de Post')
+        
     };
 
     return(
